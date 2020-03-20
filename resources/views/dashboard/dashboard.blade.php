fa1dac7ea712f5defec6e5bbd34e1ce61668b688
Adewale Adeleye <adeleyea5@gmail.com>
2020-03-20 15:55:24 +0100

    added some features

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -2,7 +2,7 @@
 
 @section('wrapper')
 	<div class="wrapper" style="min-height: 70vh;">
-            <div class="container">
+            <div class="container" >
                 <div class="row">
                    
                     <div class="col-md-12">
@@ -15,7 +15,7 @@
 			                                    <h3>
 			                                        Your Dashbord</h3>
 			                                </div>
-			                                <div class="module-body table row-fluid">
+			                                <div class="module-body table row-fluid" style="box-shadow: 5px 10px #888888;">
 			                                    <table cellpadding="0" cellspacing="0" border="0" class="datatable-1 table table-bordered table-striped	 display"
 			                                        width="100%">
 			                                        <thead>
@@ -79,7 +79,7 @@
 			                                </div>
 			                            </div>
                                     </div>
-                                    <div class="widget widget-usage span3">
+                                    <div class="widget widget-usage span3" style="box-shadow: 5px 10px #888888;">
                                     	<p><strong>Programming Proficiency</strong></p>
 	                                    <ul class="widget widget-usage unstyled span12">
 	                                        <li>
@@ -153,4 +153,79 @@
             <!--/.container-->
         </div>
         <!--/.wrapper-->
+        <script type="text/javascript">
+        	
+        	function getmore(id){
+        		if(id!==""){
+        			$.ajax({
+			        type: "GET",
+			        url: `/api/getquestion/${id}`,
+			        dataType: "json",
+			        success: function (result) {
+			            let img = '';
+			            // console.log(result[0]);
+			            var categories_options_html="";
+                       	$('#top').html(`<input class="span6" rows="10" type="text" value="${result[0].category_name}" />`);
+                       	$('#subtop').html(`<input class="span6" rows="10" type="text" value="${result[0].sub_category}" />`);
+                       	$('#quesid').html(`<textarea class="span6" rows="10" disabled type="text" style="overflow:visible; height: 50vh; max-height: 50vh;">${result[0].questions}</textarea>`);
+                       	$('#soln').html(`<textarea class="span6" rows="10" disabled type="text" >${result[0].solution}</textarea>`);
+
+
+		                $('#viewinfo').modal();
+			        },
+			        error: function (xhr, status, error) {
+			            alert("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+			        }
+			    });
+        		}
+        	}
+        </script>
+
+        <div id="viewinfo" class="modal fade" role="dialog" style="width: 45%;">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title text-center">full Detail</h4>
+                    </div>
+                    <!-- Modal Body -->
+                    <div class="modal-body">
+                         <p class="statusMsg"></p>
+                         <form role="form" >
+                         
+                        <div class="control-group">
+                        	<label class="control-label" for="basicinput">Topic</label>
+							<div class="controls" id="top" >
+								<!-- <input type="text"> -->
+                        	</div>
+                        </div>
+                        <div  class="control-group">
+                        	<label class="control-label" for="basicinput">Sub Topics</label>
+							<div class="controls" id="subtop">
+								<!-- <input type="text"> -->
+                        	</div>
+                        </div>
+                        <div  class="control-group">
+                        	<label class="control-label" for="basicinput">Question</label>
+							<div class="controls" id="quesid">
+								<!-- <input type="text"> -->
+                        	</div>
+                        </div>
+                        <div  class="control-group">
+                        	<label class="control-label" for="basicinput">Your Solution</label>
+							<div class="controls" id="soln">
+								<!-- <input type="text"> -->
+                        	</div>
+                        </div>
+                            
+                         </form>
+                     </div>
+                    <!-- Modal Footer -->
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
